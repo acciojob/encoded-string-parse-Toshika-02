0bad1e436554f12ae9314e0fb8d760ad25ae72f4
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-23 16:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const parseCode = (str) => {
   // your code here
-	const data = str.split('000');
-	if(data === 3){
-		const [firstName, lastName, id] = data;
-		return {firstName, lastName, id}
-	}else{
-		return null
-		
-		
-	}
-	
+	const zero1 = str.indexOf("0");
+  const zero2 = str.indexOf("0", zero1 + 1);
 
+  const firstName = str.substring(0, zero1);
+  const lastName = str.substring(zero1 + 1, zero2);
+  const id = str.substring(zero2 + 1);
+
+  return { firstName, lastName, id };
+	
 };
 
 // Do not change the code below
